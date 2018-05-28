440a2f30559e7e12567819fb05ca847625b573ee
Cesar <soricesar.cssm@gmail.com>
2018-05-28 08:41:40 -0500

    bean pelicula con metodos

--- src/filmstore/beans/PeliculaBean.java
+++ src/filmstore/beans/PeliculaBean.java
@@ -3,15 +3,19 @@ package filmstore.beans;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 import filmstore.dao.impl.PeliculaDAOImpl;
 import filmstore.entity.Pelicula;
+import filmstore.service.PeliculaService;
 
 @ManagedBean
 public class PeliculaBean {
 
 	private Pelicula pelicula;
-	private List<Pelicula> peliculas;
+	private DataModel<Pelicula> peliculas;
+	private PeliculaService ps = new PeliculaService();
 	
 	public Pelicula getPelicula() {
 		return pelicula;
@@ -21,13 +25,30 @@ public class PeliculaBean {
 		this.pelicula = pelicula;
 	}
 	
-	public List<Pelicula> getListarPelicula(){
-		
-		peliculas = new PeliculaDAOImpl().list();
+	public DataModel<Pelicula> getListarPelicula(){
+		List<Pelicula> pelicula = new PeliculaDAOImpl().list(); 
+		peliculas = new ListDataModel<>(pelicula);
 		
 		return peliculas;
 	}
 	
+	public String prepararAdicionarPelicula() {
+		pelicula = new Pelicula();
+		
+		return "pagina de adicionar";
+	}
 	
+	public String adicionarPelicula() {
+		ps.save(pelicula);
+		
+		return "pagina inicio";
+	}
+	
+	public String prepararModificarPelicula() {
+		List<Pelicula> pel = new PeliculaDAOImpl().list();
+		DataModel listaPeliculas = new ListDataModel<>(pel);
+		pelicula = (Pelicula)(listaPeliculas.getRowData());
+		return " ";
+	}
 	
 }
